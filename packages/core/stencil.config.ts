6b450b43949954d29acfd469e9ff605a0dfc08ff
Lucas Ribeiro <lucasnck@hotmail.com>
2022-10-12 12:09:26 -0300

    ✅ add stenciljs base config

---
+++ packages/core/stencil.config.ts
@@ -0,0 +1,27 @@
+import { Config } from '@stencil/core';
+import { reactOutputTarget as react } from '@stencil/react-output-target'
+
+export const config: Config = {
+  namespace: 'core',
+  outputTargets: [
+    react({
+      componentCorePackage: '@ds/core',
+      proxiesFile: '../react/src/components/stencil-generated/index.ts',
+      includeDefineCustomElements: true,
+    }),
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};

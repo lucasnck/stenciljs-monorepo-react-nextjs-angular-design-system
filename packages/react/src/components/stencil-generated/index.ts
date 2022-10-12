e2f02a8221e65fb8887e261e99e7f58691c7d38f
Lucas Ribeiro <lucasnck@hotmail.com>
2022-10-12 12:09:45 -0300

    ✅ add react base config with rollup

---
+++ packages/react/src/components/stencil-generated/index.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@ds/core';
+
+import { defineCustomElements } from '@ds/core/loader';
+
+defineCustomElements();
+export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');

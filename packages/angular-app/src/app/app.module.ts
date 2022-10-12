7f92211e757f8912d80e3b72a7136965d3936de9
Lucas Ribeiro <lucasnck@hotmail.com>
2022-10-12 17:39:59 -0300

    🐛 fix angular imports

--- packages/angular-app/src/app/app.module.ts
+++ packages/angular-app/src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { CoreComponentsModule } from '../../../angular/src/core-components.module';
+import { DsLibraryModule } from '@ds/angular/dist/ds-library';
 
 // import { applyPolyfills, defineCustomElements } from '@ds/core/loader';
 
@@ -20,10 +20,10 @@ import { CoreComponentsModule } from '../../../angular/src/core-components.modul
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    CoreComponentsModule
+    DsLibraryModule
   ],
   providers: [],
-  // schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 

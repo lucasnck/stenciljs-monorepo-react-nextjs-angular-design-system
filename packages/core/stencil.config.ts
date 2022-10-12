c41b9dcf4a7871fadee6e87b445f73677bfda2e0
Lucas Ribeiro <lucasnck@hotmail.com>
2022-10-12 16:19:11 -0300

    ✅ angular output target

--- packages/core/stencil.config.ts
+++ packages/core/stencil.config.ts
@@ -1,5 +1,6 @@
 import { Config } from '@stencil/core';
 import { reactOutputTarget as react } from '@stencil/react-output-target'
+import { angularOutputTarget } from '@stencil/angular-output-target';
 
 export const config: Config = {
   namespace: 'core',
@@ -8,6 +9,11 @@ export const config: Config = {
       componentCorePackage: '@ds/core',
       proxiesFile: '../react/src/components/stencil-generated/index.ts',
       includeDefineCustomElements: true,
+    }),
+    angularOutputTarget({
+      componentCorePackage: '@ds/core',
+      directivesProxyFile: '../angular/src/lib/stencil-generated/components.ts',
+      directivesArrayFile: '../angular/src/lib/stencil-generated/index.ts',
     }),
     {
       type: 'dist-custom-elements',
